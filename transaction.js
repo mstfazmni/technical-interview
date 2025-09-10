134bd0900acd445ad3c47d3736f9ac9a4ca98e58
Mostafa Zamani <119905696+mstfazmni@users.noreply.github.com>
2025-09-10 16:45:51 -0600

    create transaction

---
+++ transaction.js
@@ -0,0 +1,29 @@
+const { use } = require("react");
+
+const transactions = [
+  { userId: 1, amount: 50 },
+  { userId: 1, amount: 200 },
+  { userId: 2, amount: 500 },
+  { userId: 2, amount: 40 },
+  { userId: 3, amount: 300 }
+];
+
+  const aboveThreshholdTranas = transactions.filter((transaction) => transaction.amount >= 100);
+// console.log(aboveThreshholdTranas);
+
+const TranGetter = aboveThreshholdTranas.reduce((acc, tran) => {
+    const {userId, amount} = tran;
+  
+
+    if (acc[userId]) {
+        acc[userId].count += 1;
+        acc[userId].total += amount;
+    } else {
+        acc[userId] = {count: 1, total: amount};
+    }
+
+    return acc;
+}, {})
+
+console.log(TranGetter)
+
